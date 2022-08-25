d0fdd5e27b4a8df00908b45f784447e189a10a69
dengxinjuan <dengxinj@gmail.com>
2022-08-24 19:34:21 -0700

    hi

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 
 export default function App() {
+  OneSignal.setAppId("4a3d23c8-8d8a-4f84-acf9-23146df6b98e");
+  OneSignal.promptForPushNotificationsWithUserResponse(response => {
+    console.log("Prompt response:", response);
+  });
   return (
     <View style={styles.container}>
-      <Text>Hello World
-        
-        !</Text>
+      <Text>I need to create a prompt button to prompt for ios
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
