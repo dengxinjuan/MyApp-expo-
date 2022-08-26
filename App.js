3c591c1f9d7f722ac942b7de66f4e13e892ca40d
dengxinjuan <dengxinj@gmail.com>
2022-08-25 20:58:12 -0700

    first commit

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button,Alert } from 'react-native';
 import OneSignal from 'react-native-onesignal';
 
 export default function App() {
   OneSignal.setAppId("4a3d23c8-8d8a-4f84-acf9-23146df6b98e");
+  OneSignal.setLogLevel(6, 0);
+  const deviceState = OneSignal.getDeviceState();
+  
   OneSignal.promptForPushNotificationsWithUserResponse(response => {
     console.log("Prompt response:", response);
+    console.log(deviceState);
   });
   return (
     <View style={styles.container}>
-      <Text>the ios bundle mismatch. I dont know why~
-      </Text>
+      <Text>I couldnt built on real device. very annoying!</Text>
+      <Text>Your player Id is {deviceState[0]}</Text>
       <StatusBar style="auto" />
+      <View>
+        <Button title="Left button" onPress={() => OneSignal.setExternalUserId('123457890')} />
+        <Button title="Right button" onPress={() => OneSignal.setEmail('dengxinj@msu.edu')} />
+      </View>
     </View>
   );
 }
