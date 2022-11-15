46e66ea86dd1922ed783b4e89e35bd15ff0de409
dengxinjuan <dengxinj@gmail.com>
2022-11-14 22:00:14 -0800

    press button

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Button,Alert } from 'react-native';
 import OneSignal from 'react-native-onesignal';
 
+const image = require('./');
+
 export default function App() {
   OneSignal.setAppId("4a3d23c8-8d8a-4f84-acf9-23146df6b98e");
   OneSignal.setLogLevel(6, 0);
@@ -13,13 +15,20 @@ export default function App() {
   });
   return (
     <View style={styles.container}>
-      <Text>I couldnt built on real device. very annoying!</Text>
+      <Text>I receive ios notification! it is cool!::</Text>
       <Text>Your player Id is {deviceState[0]}</Text>
       <StatusBar style="auto" />
       <View>
-        <Button title="Left button" onPress={() => OneSignal.setExternalUserId('123457890')} />
-        <Button title="Right button" onPress={() => OneSignal.setEmail('dengxinj@msu.edu')} />
+        <Button title="set external user id" onPress={() => OneSignal.setExternalUserId('123457890')} />
+        <Button title="set email" onPress={() => OneSignal.setEmail('dengxinju@msu.edu')} />
+        <Button title="Tag User" onPress={() => OneSignal.sendTag("hello","world")} />
       </View>
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+    <Image
+      source={image}
+      style={{ width: 100, height: 100 }}
+    />
+  </View>
     </View>
   );
 }
